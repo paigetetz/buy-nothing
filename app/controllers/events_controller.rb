a3860f0324ecaade2ae6cea991581e7cd50915ac
paige <paigetetz@gmail.com>
2023-02-25 15:59:23 -0500

    changes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,13 @@
 class EventsController < ApplicationController
   skip_before_action :authorize
+  def index
+    render json: Event.all
+  end
+  def show
+    event = Event.find_by_id(params[:id])
+    if event.valid?
+      render json: event, status: :created
+      
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_25_201033) do
     t.string "name"
     t.string "description"
     t.string "location"
+    t.date "date"
     t.string "time"
     t.integer "user_id"
     t.datetime "created_at", null: false
@@ -42,6 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_25_201033) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
+    t.string "profile_pic"
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
