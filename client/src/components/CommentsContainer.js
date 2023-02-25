c52db0d877dca5ccbb9cb52cb96b7a010e132664
Ceri Dorsey-Tyler <cerimay@gmail.com>
2023-02-25 18:35:23 -0500

    components

---
+++ client/src/components/CommentsContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CommentsContainer(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default CommentsContainer;
---
+++ client/src/components/UserLogin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UserLogin(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default UserLogin;
