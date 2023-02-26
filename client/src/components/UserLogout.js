ceaa42590d881b393cc010d01b5d9bbe89bf34fe
Ceri Dorsey-Tyler <cerimay@gmail.com>
2023-02-25 19:27:29 -0500

    auth

--- client/src/components/UserLogout.js
+++ client/src/components/UserLogout.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-function UserLogout(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+function UserLogout({ user, setUser }) {
+  let navigate = useNavigate();
+
+  function handleLogoutClick() {
+    fetch("/logout", { method: "DELETE" }).then((r) => {
+      if (r.ok) {
+        setUser(null);
+        navigate("/");
+      }
+    });
+  }
+
+  return (
+    <div className="logout">
+      <h1>Are you sure you want to log out?</h1>
+      <button className="logout-link" onClick={handleLogoutClick}>
+        Log Out
+      </button>
+      <Link to="/listings">
+        Go Back
+      </Link>
+    </div>
+  );
 }
 
 export default UserLogout;
